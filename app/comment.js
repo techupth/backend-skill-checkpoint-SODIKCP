8a647cd0b5abe7c56044fb4b4581e4ea5e5156d2
Sodikcp <paramesh.rah@gmail.com>
2024-01-17 11:33:39 +0700

    feat: fix comment app

--- app/comment.js
+++ app/comment.js
@@ -8,28 +8,39 @@ commentRouter.post("/:id/comments", async (req, res) => {
   const collection = db.collection("posts");
   const postId = new ObjectId(req.params.id);
   const commentData = { ...req.body };
-  const comment = await collection.updateOne(
-    {
-      _id: postId,
-    },
-    {
-      $push: { comments: commentData },
-    }
-  );
-
+  const commentId = new ObjectId(req.params.id);
   /**modifiedCount เอาไว้รายงานจำนวนเอกสารที่ถูกปรับเปลี่ยน ใช้กับพวก
    * updateOne, updateMany, deleteOne, หรือ deleteMany ถ้ามีการเปลี่ยนแปลง
    * modifiedCountจะเท่ากับ 1 แต่ถ้าไม่มี modifiedCountจะ = 0
    */
-  if (comment.modifiedCount === 1) {
-    res.status(202).json({
-      message: `Comment in post id ${postId} has been added successfully`,
-    });
-  } else {
+  // if (comment.modifiedCount === 1) {
+  //   res.status(202).json({
+  //     message: `Comment in post id ${postId} has been added successfully`,
+  //   });
+  // } else {
+  //   res.status(404).json({
+  //     message: `Post not found`,
+  //   });
+  // }
+  if (commentData.comments.length > 300) {
+    return res.status(400).json({ message: "comment is too long" });
+  }
+  const comment = await collection.updateOne(
+    { _id: postId },
+    { $push: { comments: { ...commentData, _id: commentId } } }
+  );
+  return res.json({ message: "complete" });
+});
+
+commentRouter.get("/:id/", async (req, res) => {
+  try {
+    const collection = db.collection("comments");
+    const postId = req.params.id;
+    await collection.find().toArray();
+  } catch {
     res.status(404).json({
-      message: `Post not found`,
+      message: "Server error",
     });
   }
 });
-
 export default commentRouter;
